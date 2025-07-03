79fe1b63ed4a37a29c16355bb4d9d406101fd3f6
Oliver Schafeld <oliver@schafeld.com>
2025-07-03 17:01:11 +0200

    set up routes

--- atm-user-interface/src/app/app.routes.ts
+++ atm-user-interface/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Balance } from './balance/balance';
+import { Withdrawal } from './withdrawal/withdrawal';
+import { Deposit } from './deposit/deposit';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'balance', component: Balance },
+  { path: 'withdraw', component: Withdrawal },
+  { path: 'deposit', component: Deposit },
+  { path: '', redirectTo: '/balance', pathMatch: 'full' }
+];
