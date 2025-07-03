bc1352e2ef70dd1f0021e71c12136242c0113f52
Oliver Schafeld <oliver@schafeld.com>
2025-07-03 17:30:47 +0200

    add home routes, link deposit-coins and -notes components

--- atm-user-interface/src/app/app.routes.ts
+++ atm-user-interface/src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
 import { Balance } from './balance/balance';
 import { Withdrawal } from './withdrawal/withdrawal';
 import { Deposit } from './deposit/deposit';
+import { DepositCoins } from './deposit-coins/deposit-coins';
+import { DepositNotes } from './deposit-notes/deposit-notes';
 
 export const routes: Routes = [
+  { path: '', component: Home },
   { path: 'balance', component: Balance },
   { path: 'withdraw', component: Withdrawal },
   { path: 'deposit', component: Deposit },
-  { path: '', redirectTo: '/balance', pathMatch: 'full' }
+  { path: 'deposit-coins', component: DepositCoins },
+  { path: 'deposit-notes', component: DepositNotes }
 ];
